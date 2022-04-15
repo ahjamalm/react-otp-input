f5f44f3694d630eb0ba4cc193260c02d5bc32f49
Roopali Singh <67934617+roopali-singh@users.noreply.github.com>
2022-04-15 19:40:57 +0530

    Fix: remove last box selection in ios chrome attempt2.0

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -253,12 +253,12 @@ class OtpInput extends Component {
     let value = e.target.value;
     if (this.isInputValueValid(value)) {
       this.focusNextInput();
-    } else if (value && value.trim().length > 0) {
+    } else if (value && value.trim().length > 1) {
       // This is a workaround for dealing mobile chrome does not fire onPaste event from sms auto-populate.
 
-      e.clipboardData = {
-        getData: () => value.trim(),
-      };
+//       e.clipboardData = {
+//         getData: () => value.trim(),
+//       };
       this.handleOnPaste(e);
     } else {
       // This is a workaround for dealing with keyCode "229 Unidentified" on Android.
